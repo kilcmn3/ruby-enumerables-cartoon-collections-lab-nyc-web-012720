72f24c8c9f795542b6a955d906f05445b4d38ce7
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-14 07:11:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,4 +21,5 @@ end
 def find_the_cheese(contains_cheddar)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  contains_cheddar.find(cheese_types) {|a, b| a == b}
 end
