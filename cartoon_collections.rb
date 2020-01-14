2a9c38667845fcd9bdb2eb25d4987c1ac990ed78
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-14 07:00:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 def long_planeteer_calls(calls_long)# code an argument here
   # Your code here
 calls_long.sort do |x|
-    x.length > 4 ? true : false
+    x.length>4 ? true : false
   end
 end
 
